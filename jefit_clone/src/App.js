1e3eb2a6845b9273c22ead207e6781ea136e4179
unknown <yadav06bihari@gmail.com>
2022-12-14 20:46:45 +0530

    day_1

---
+++ jefit_clone/src/App.js
@@ -0,0 +1,19 @@
+
+import './App.css';
+import Navbar from './Components/Navbar';
+import Register from './Components/Pages/Register';
+import Allroutes from './Components/Routes/Allroutes';
+
+function App() {
+  return (
+   <div>
+   
+  <Navbar/>
+  <Allroutes/>
+  
+
+    </div>
+  );
+}
+
+export default App;
---
+++ jefit_clone/src/Components/Pages/Coach.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const Coach = () => {
+  return (
+    <div>Coach</div>
+  )
+}
+
+export default Coach
